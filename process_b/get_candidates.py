b94bf658adf671598800e43bb991daf559364f6e
Kim Nguyen <kimnguyenjbnds@gmail.com>
2017-08-14 12:08:47 -0400

    Added exon count filter to candidates.py

--- process_b/get_candidates.py
+++ process_b/get_candidates.py
@@ -9,23 +9,36 @@ strange = ''
 
 
 for dir_name in data:
-	go = True
 	sys.stdout.write(dir_name + '\n')
 	try:
 		rna = '-'.join(dir_name.split('/')[-1].split('_')[-3].split('-')[0:-1])
 		dir_ID = dir_name.split('/')[-1].split('_')[-3].split('-')[-1]
 
 		flag_file = dir_name + '/' + rna + '-' + dir_ID + '_analysis/flag_' + rna + '-' + dir_ID + '.csv'
-
 		flag_df = text_to_df(flag_file,index='ID')
+
+		ss_file = dir_name + '/' + rna + '-' + dir_ID + '_analysis/ss_' + rna + '-' + dir_ID + '.csv'
+		ss_df = text_to_df(ss_file,index='ID')
+
+		# Filters to ensure that the maximum number of exons for an isoform is at least 5 and min is at least 3
+		# This is the simplest case for NMD
+		# The 5 max is 1-2-3-4-5, which has lengths mod 3: 0-1-2-0-0
+		# The 3 min is 1- - -4-5, which has lengths mod 3: 0- - -0-0
+
+		max_exons = max(map(int,list(ss_df['exonCount'])))
+		min_exons = min(map(int,list(ss_df['exonCount'])))
+		if (max_exons - min_exons) >= 2:
+		    go = True
+		else:
+			sys.stdout.write('\tEXON COUNT FILTERED OUT ' + dir_name + '\n')
 	
 	except:
-		sys.stdout.write('\tEXCEPT ' + dir_name + '\n\n')
+		sys.stdout.write('\tFILE READ ERROR ' + dir_name + '\n\n')
 		strange += rna + '\t' + dir_name + '\n'
 		go = False
 
 	if go:
-		sys.stdout.write('\tWORKED ' + dir_name + '\n')
+		sys.stdout.write('\tPASSED FILTER ' + dir_name + '\n')
 		has_3utr = False
 
 		utr3_inds = list(set(flag_df['exonsFromEnd']))
@@ -34,14 +47,7 @@ for dir_name in data:
 		utr3_inds = sorted(utr3_inds)
 		if utr3_inds[-1] > 0:
 			has_3utr = True
-		"""
-		for i in utr3_inds:
-			sys.stdout.write('\t\tUTR Test Loop\n')
-			if int(i) != 0:
-				has_3utr = True
-				sys.stdout.write('\t\tUTR Test Loop Before Break\n')
-				break
-		"""
+
 		sys.stdout.write('\t\tHas 3\' UTR? ' + str(has_3utr) + '\n')
 		
 		nmd_diverse = False
